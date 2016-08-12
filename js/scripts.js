e8a818d639b0274e3f03b8070cf7fe0675d65ed2
Anand Angalig <Guest@Epicoduss-iMac-7.local>
2016-08-12 15:17:15 -0400

    add branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
-  $
+  $("form#survey").submit(function(event) {
+    event.preventDefault();
+    // debugger;
+    var name = $("input#name").val();
+    var email = $("input#email").val();
+    var skillLevel = $("#skillLevel").val();
+    var why = $("input#why").val();
+    var popular = $("#popular").val();
+    var frontOrBack = $("input:radio[name=frontOrBack]:checked").val();
 
-
+    if (frontOrBack === "frontEnd") {
+      $("#cssDesign").show();
+    } else if (frontOrBack === "backEnd" && popular === "cSharp") {
+      $("#cSharpJava").show();
+    } else if (frontOrBack === "backEnd" && popular === "ruby") {
+      $("#rubyRails").show();
+    } else if (frontOrBack === "backEnd" && popular === "java") {
+      $("#androidJava").show();
+    }
+  });
 });
